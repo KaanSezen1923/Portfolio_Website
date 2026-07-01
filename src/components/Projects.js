8e3f74cd2ebcf443e23cbcfca2c8c68960a0d395
Kaan Sezen <119515258+KaanSezen1923@users.noreply.github.com>
2026-07-01 20:55:45 +0300

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,6 @@ import project1 from "../assets/project1.png";
 import project2 from "../assets/project2.png";
 import project3 from "../assets/project3.png";
 import project4 from "../assets/project4.png";
-import project5 from "../assets/project5.png";
 import project6 from "../assets/project6.png";
 import project7 from "../assets/project7.png";
 import project8 from "../assets/project8.jpeg";
